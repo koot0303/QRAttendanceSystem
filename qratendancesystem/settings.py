67fe656abc9dc4285cd874b2c24cc3333eb68c50
koot0303 <sashou.t0303@gmail.com>
2025-04-18 19:42:38 +0900

    Update setting.py

--- qratendancesystem/settings.py
+++ qratendancesystem/settings.py
@@ -133,5 +133,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/apps/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR / "apps", "static")
